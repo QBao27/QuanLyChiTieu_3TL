69a22208b6dde8a2300791b4b927069f75e78845
BaoAnh79 <nbanhcntt2211026@student.ctuet.edu.vn>
2025-06-30 15:21:43 +0700

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'views/TrangChu/TrangChu.dart';
+import 'views/ThongKe/ThongKe.dart';
+import 'views/Lich/Lich.dart';
+import 'views/TaiKhoan/TaiKhoan.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +17,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'QuanLyChiTieu_3TL',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
       ),
-      home: const MyHomePage(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    TrangChu(),
+    ThongKe(),
+    Lich(),
+    TaiKhoan()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quản Lý Chi Tiêu', style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Trang chủ'),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Thống kê'),
+          BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: 'Lịch'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Tài khoản'),
+        ],
+      ),
     );
   }
 }
---
+++ lib/views/TrangChu/TrangChu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrangChu extends StatefulWidget {
+  const TrangChu({super.key});
+
+  @override
+  State<TrangChu> createState() => _TrangChuState();
+}
+
+class _TrangChuState extends State<TrangChu> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("hello"),);
+  }
+}
